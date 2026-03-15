c5a713fdf239f244f2f27e96e3df3e2842ec3044
Drishti Aggarwal <drishtiaggarwals17@gmail.com>
2026-03-15 12:44:46 +0530

    basic set up and installations

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
+import "dotenv/config";
 import express from "express";
-import type {Request,Response} from "express";
+import type { Request, Response } from "express";
+import { prisma } from "./config/prisma";
 
+const app = express()
+const port: number = 8080
 
-const app = express();
-const port : Number = 8080
-
-app.get("/",(req : Request,res : Response)=>{
-    res.status(200).send("working fine!")
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("working fine!")
 })
 
-app.listen(port,()=>{
-    console.log(`server is running at http://localhost:${port} `)
+app.get("/db-test", async (req: Request, res: Response) => {
+  try {
+    const result = await prisma.$queryRaw`SELECT NOW()`
+    res.json(result)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Database connection failed")
+  }
+})
+
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`)
 })
